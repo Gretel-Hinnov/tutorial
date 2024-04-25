f8f9552e5a2b779e616090ef66c69eff36549b6a
opilane <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-04-25 10:18:12 +0300

    tut

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeTestForGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github test :)");
+            Console.WriteLine("yayyy");
+        }
+    }
+}
